6221236372a24ae0c95405f018a95fcbf436a908
lebogangMatlala <lebogangmatlala94@gmail.com>
2025-06-18 10:42:05 +0200

    Add simple express hello world API

--- index.js
+++ index.js
@@ -5,7 +5,17 @@ app.get('/', (req, res) => {
   res.send('Hello from Node.js API!');
 });
 
+// New route: /info returns JSON with app details
+app.get('/info', (req, res) => {
+  res.json({
+    appName: "My Node API",
+    version: "1.0.0",
+    description: "A simple Node.js API running on Docker & Kubernetes"
+  });
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
